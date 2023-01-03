9c4587426c25af5c2025e00ccf7eb98c9e99d77c
Richard W <104865168+Justacanuk@users.noreply.github.com>
2023-01-03 16:10:28 -0500

    2nd

---
+++ frontend/constants.js
@@ -0,0 +1,2 @@
+export const SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/justacanuk/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0xeD1c1697873E5B78f3d318404f71D14b25e64f4E";
